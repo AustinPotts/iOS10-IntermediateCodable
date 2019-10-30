a97b2cf2f7ee6b1134fb08db00977a0148cf2d4d
AustinPotts <pottsevolvements@gmail.com>
2019-10-30 16:28:56 -0400

    Project Completed

--- Pokemon.playground/Contents.swift
+++ Pokemon.playground/Contents.swift
@@ -6,20 +6,74 @@ var str = "Hello, playground"
 struct Pokemon: Codable {
     
     enum PokemonKeys: String, CodingKey {
-        case pokemonName = "name"
-        case height
+        case name
+        case species
+        case abilities
+        
+        enum SpeciesKeys: String, CodingKey {
+              case name
+          }
+        enum AbilityDescriptionKeys: String, CodingKey {
+            case ability
+            
+            enum AbilityKeys: String, CodingKey {
+                case name
+            }
+        }
+        
     }
     
-    let pokemonName: String
-    let height: String //Int in the JSOn but we want a string
+  
+    
+    let name: String
+    let species: String
+    let abilities: [String]
+    
     
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: PokemonKeys.self)
         
-        pokemonName = try container.decode(String.self, forKey: .pokemonName)
+        name = try container.decode(String.self, forKey: .name)
+       
+        let speciesContainer = try container.nestedContainer(keyedBy: PokemonKeys.SpeciesKeys.self, forKey: .species)
         
-        let heightInt = try container.decode(Int.self, forKey: .height)
-        height = String(heightInt)
+        species = try speciesContainer.decode(String.self, forKey: .name)
+        
+        var abilitiesContainer = try container.nestedUnkeyedContainer(forKey: .abilities)
+        
+        var abilityNames: [String] = []
+        
+        while abilitiesContainer.isAtEnd == false {
+            let abilityDescriptionContainer = try abilitiesContainer.nestedContainer(keyedBy: PokemonKeys.AbilityDescriptionKeys.self)
+            
+            let abilityContainer = try abilityDescriptionContainer.nestedContainer(keyedBy: PokemonKeys.AbilityDescriptionKeys.AbilityKeys.self, forKey: .ability)
+            
+            let abilityName = try abilityContainer.decode(String.self, forKey: .name)
+            abilityNames.append(abilityName)
+        }
+        
+        abilities = abilityNames
+    }
+    
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: PokemonKeys.self)
+        
+        try container.encode(name, forKey: .name)
+        
+        var speciesContainer = container.nestedContainer(keyedBy: PokemonKeys.SpeciesKeys.self, forKey: .species)
+        
+        try speciesContainer.encode(species, forKey: .name)
+        
+        var abiltiesContainer = container.nestedUnkeyedContainer(forKey: .abilities)
+        
+        for ability in abilities {
+            var abiltiesDescriptionContainer = abiltiesContainer.nestedContainer(keyedBy: PokemonKeys.AbilityDescriptionKeys.self)
+            
+            var abilityContainer = abiltiesDescriptionContainer.nestedContainer(keyedBy: PokemonKeys.AbilityDescriptionKeys.AbilityKeys.self, forKey: .ability)
+            
+            try abilityContainer.encode(ability, forKey: .name)
+        }
     }
     
 }
@@ -29,3 +83,12 @@ let data = try! Data(contentsOf: url)
 
 let decoder = JSONDecoder()
 let charmander = try! decoder.decode(Pokemon.self, from: data)
+
+
+let encoder = JSONEncoder()
+
+encoder.outputFormatting = .prettyPrinted
+let charmanderData = try! encoder.encode(charmander)
+
+let dataAsString = String(data: charmanderData, encoding: .utf8)!
+print(dataAsString)
--- iOS-Starwars.playground/Contents.swift
+++ iOS-Starwars.playground/Contents.swift
@@ -8,19 +8,74 @@ struct Person: Codable {
         case name
         case height
         case hairColor = "hair_color"
+        
+        case films
+        case starships
     }
     
     let name: String
     let height: Int
     let hairColor: String
     
+    let films: [URL]
+    let starships: [URL]
+    
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: PersonKeys.self)
         
         name = try container.decode(String.self, forKey: .name)
         hairColor = try container.decode(String.self, forKey: .hairColor)
-        let heightString = try container.decode(String.self, forKey: .height)
-        height = Int(heightString) ?? 0
+        
+        do {
+            height = try container.decode(Int.self, forKey: .height)
+        } catch {
+            let heightString = try container.decode(String.self, forKey: .height)
+            height = Int(heightString) ?? 0
+        }
+        
+        
+        
+        
+    //    let filmString = container.decode([String].self, forKey: .films)
+        
+        var filmsContainer = try container.nestedUnkeyedContainer(forKey: .films)
+        var filmURLs: [URL] = []
+        
+        
+        //One way to decode
+        while filmsContainer.isAtEnd == false {
+            let filmsString = try filmsContainer.decode(String.self)
+            if let filmURL = URL(string: filmsString) {
+                filmURLs.append(filmURL)
+            }
+        }
+        
+        films = filmURLs
+        
+        //One way decode
+        starships = try container.decode([URL].self, forKey: .starships)
+        
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: PersonKeys.self)
+        
+        let heightString = String(height)
+        try container.encode(heightString, forKey: .height)
+        
+        try container.encode(name, forKey: .name)
+        
+        try container.encode(hairColor, forKey: .hairColor)
+        try container.encode(films, forKey: .films)
+       // try container.encode(starships, forKey: .starships)
+      
+//        var starshipsContainer = container.nestedUnkeyedContainer(forKey: .starships)
+//        for starship in starships {
+//           try starshipsContainer.encode(starship.absoluteString)
+//        }
+        
+        try container.encode(starships.map {$0.absoluteString}, forKey: .starships)
+        
     }
 }
 
@@ -29,4 +84,13 @@ let url = URL(string: "https://swapi.co/api/people/1/")!
 let data = try! Data(contentsOf: url)
 
 let decoder = JSONDecoder()
+
 let luke = try! decoder.decode(Person.self, from: data)
+
+let encoder = JSONEncoder()
+
+encoder.outputFormatting = .prettyPrinted
+let lukeData = try! encoder.encode(luke)
+
+let dataAsString = String(data: lukeData, encoding: .utf8)!
+print(dataAsString)
